a484ad9cc6c4956b5bc2b4ab99e6bea16ed050f9
jpalomaki <jukka.palomaki@vincit.fi>
2017-12-05 00:57:09 +0200

    Refactor

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+my_env = os.getenv('MY_ENV', 'undefined')
+
 @app.route('/')
 def hello():
-  return 'Hello, Flaky!'
+  return 'Hello, Flaky! (MY_ENV: {})'.format(my_env)
 
 @app.route('/healthz')
 def healthz():
